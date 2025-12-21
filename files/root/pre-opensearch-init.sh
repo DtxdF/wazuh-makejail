aafc7e3fbf5b582d27ba10f09b0afd3cda3b301c
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-12-20 21:36:11 -0400

    Improvements
    * Allow any networking option.
    * Fix logstash-output-opensearch installation.
    * Don't expose ports by default.
    * Add persist to template.

--- files/root/pre-opensearch-init.sh
+++ files/root/pre-opensearch-init.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-#
+
+logstash_output_opensearch_ver=2.0.3
+
 echo " "
 echo -e "\e[1;37m Installing logstash-output-opensearch plugin ...\e[0m"
 echo " "
 
-cd /usr/local/logstash/bin; sh -c "JAVA_HOME=/usr/local/openjdk17 ./logstash-plugin install logstash-output-opensearch"
+fetch -qo "/tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}.tar.gz" "https://github.com/opensearch-project/logstash-output-opensearch/releases/download/${logstash_output_opensearch_ver}/artifacts.tar.gz" || exit $?
+mkdir -p "/tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}" || exit $?
+tar -C "/tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}" -xf /tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}.tar.gz || exit $?
+
+cd /usr/local/logstash/bin; sh -c "DEBUG=1 JAVA_HOME=/usr/local/openjdk17 ./logstash-plugin install /tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}/dist/logstash-output-opensearch-${logstash_output_opensearch_ver}-java.gem" || exit $?
+
+rm -rf "/tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}"
+rm -f "/tmp/logstash-output-opensearch-${logstash_output_opensearch_ver}.tar.gz"
 
 echo " "
 echo -e "\e[1;37m Copy opensearch config sample files ...\e[0m"
 echo " "
 
-cd /usr/local/etc/opensearch/opensearch-security; sh -c 'for i in $(ls *.sample ) ; do cp -p ${i} $(echo ${i} | sed "s|.sample||g") ; done'
+cd /usr/local/etc/opensearch/opensearch-security; sh -c 'for i in $(ls *.sample ) ; do cp -p ${i} $(echo ${i} | sed "s|.sample||g") ; done' || exit $?
