423ebfaffd25985efc9211a3dbc442bd8312be59
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-04-07 15:01:37 -0400

    Many changes
    * indexer:
      - wazuh-template.json not correctly downloaded based on the current
        version of Wazuh. Now pkg-rquery(8) is used to get the current
        version.
      - Set opensearch_java_home and logstash_java_home parameters.
        By default, sysutils/logstash8 is set to openjdk21 in its java_home
        and this ins't correct due to incompatibility.
      - Merge logstash.conf from security/wazuh-server.
    * manager:
      - Update filebeat module to 0.5.
      - Install serpico's rules in this deployment.

--- manager/user-files/entrypoint-scripts/001-fix-permissions.sh
+++ manager/user-files/entrypoint-scripts/001-fix-permissions.sh
@@ -6,3 +6,5 @@ chmod 640 /var/ossec/etc/authd.pass
 chown root:wazuh /var/ossec/etc/authd.pass
 chmod 660 /var/ossec/etc/ossec.conf
 chown root:wazuh /var/ossec/etc/ossec.conf
+chown wazuh:wazuh /var/ossec/etc/rules/serpico_rules.xml
+chmod 660 /var/ossec/etc/rules/serpico_rules.xml
